022dac186b552f1aa5aa310c1b18f522236a6e08
cp361 <116882875+cp361@users.noreply.github.com>
2022-12-12 01:01:13 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+let navbar = document.getElementById('navbar');
+let navbarLogo = document.getElementById('open-burger-menu');
+
+navbarLogo.onclick = function () {
+	navbar.classList.toggle('open');
+
+}
+
+
+
+var menu = document.getElementById('menu');
+
+menu.onclick = function() {
+    menu.classList.toggle('openmenu');
+}
+
+
+
